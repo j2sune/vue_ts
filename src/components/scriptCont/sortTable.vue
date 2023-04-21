857b61985b6ecb5ddfade39934903ee1fa8fdd9b
jslee <js080825@gmail.com>
2023-04-21 10:51:29 +0900

    [modify] sort end

--- src/components/scriptCont/sortTable.vue
+++ src/components/scriptCont/sortTable.vue
@@ -42,6 +42,10 @@
     date: number
   }
 
+  /* 테이블 정렬 */
+  let sortType = 'init'
+  let existItemId = 'none'
+
   const thCont:thType = { score: '순위', name: '음원명', artist: '가수', date: '발매일' }
 
   const sortData: dataType[] = [
@@ -57,140 +61,128 @@
     { score: 4, name: '신호등', artist: '이무진', date: 20210819 }
   ]
 
+  function tableInsert(tableValue:dataType[]) {
+    let tr = ''
+    tableValue.forEach((item:dataType) => {
+      tr += '<tr>'
+      + '<td>' + item.score + '</td>'
+      + '<td>' + item.name + '</td>'
+      + '<td>' + item.artist + '</td>'
+      + '<td>' + item.date + '</td>'
+      + '</tr>'
+    })
+
+    return tr
+  }
+
+  const tableOrig = tableInsert(sortData)
+
   function sorting(e:MouseEvent, clickId:string) {
     let tbody:Element | null | undefined = (e.target as HTMLButtonElement)?.closest('thead')?.nextElementSibling;
-
-    function getId(id:string) {
-      let nowClick:string;
-      if (id == 'score') {
-        const name = "score"
-        nowClick = typeof sortData[0][name];
-      } else if (id == 'name') {
-        const name = "name"
-        nowClick = typeof sortData[0][name];
-      } else if (id == 'artist') {
-        const name = "artist"
-        nowClick = typeof sortData[0][name];
-      } else {
-        const name = "date"
-        nowClick = typeof sortData[0][name];
-      }
-      return nowClick
-    }
-    const itemType:string = getId(clickId)
-    console.log(itemType)
-
-    function tableM(tableValue:dataType[]) {
-      let tr = ''
-      tableValue.forEach((item:dataType) => {
-        tr +=
-          '<tr>' +
-          '<td>' +
-          item.score +
-          '</td>' +
-          '<td>' +
-          item.name +
-          '</td>' +
-          '<td>' +
-          item.artist +
-          '</td>' +
-          '<td>' +
-          item.date +
-          '</td>' +
-          '</tr>'
-      })
-
-      return tr
-    }
+    let sortFc:number;
+    let result:dataType[];
 
     if (tbody != null) {
-      tbody.innerHTML = tableM(sortData)
+      tbody.innerHTML = tableInsert(sortData)
     }
 
-    /* 테이블 정렬 */
-    let sortType = 'init'
-    let existItemId = 'none'
+    async function sortTable(clickId:string) {
 
-    if (existItemId == clickId) {
-      sortTable()
-    } else {
-      sortType = 'init'
-      sortTable()
-    }
+      if (existItemId != clickId) {
+        sortType = 'init'
+      }
 
-    function sortTable() {
-      let rdd:number;
-      if (itemType == 'number') {
-        if (sortType === 'init') {
-          sortType = 'asc'
-          sortData.sort((a:dataType, b:dataType) :number => {
-            if (clickId == 'score') {
-              const type22 = "score"
-              rdd = a[type22] - b[type22]
-            } else {
-              const type22 = "date"
-              rdd = a[type22] - b[type22]
-            }
-            return rdd
-          })
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        } else if (sortType === 'asc') {
-          sortType = 'desc'
-          sortData.sort((a:dataType, b:dataType) :number => {
-            if (clickId == 'score') {
-              const type22 = "score"
-              rdd = b[type22] - a[type22]
-            } else {
-              const type22 = "date"
-              rdd = b[type22] - a[type22]
-            }
-            return rdd
-          })
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        } else {
-          sortType = 'init'
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        }
-      } /*else if (itemType == 'string') {
-        if (sortType === 'init') {
-          sortType = 'asc'
-          sortData.sort(function (a, b) {
-            let x = a[clickId].toLowerCase()
-            let y = b[clickId].toLowerCase()
-
+      if (clickId == 'score') {
+        const name = "score"
+        result = sortData.sort((a:dataType, b:dataType) :number => {
+          if (sortType === 'init') {
+            sortFc = a[name] - b[name]
+          } else if (sortType === 'asc') {
+            sortFc = b[name] - a[name]
+          } 
+          return sortFc
+        })
+      } else if (clickId == 'name') {
+        const name = "name"
+        result = sortData.sort((a:dataType, b:dataType) :number => {
+          if (sortType === 'init') {
+            let x = a[name].toLowerCase()
+            let y = b[name].toLowerCase()
             if (x < y) {
               return -1
             }
-
             if (x > y) {
               return 1
             }
-            return 0
-          })
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        } else if (sortType === 'asc') {
-          sortType = 'desc'
-          sortData.sort(function (a, b) {
-            let x = a[clickId].toLowerCase()
-            let y = b[clickId].toLowerCase()
-
+            return 0 
+          } else if (sortType === 'asc') {
+            let x = a[name].toLowerCase();
+            let y = b[name].toLowerCase();
             if (x > y) {
+              return -1;
+            }
+    
+            if (x < y) {
+              return 1;
+            }
+            return 0;
+          }
+          return 0
+        })
+      } else if (clickId == 'artist') {
+        const name = "artist"
+        result = sortData.sort((a:dataType, b:dataType) :number => {
+          if (sortType === 'init') {
+            let x = a[name].toLowerCase()
+            let y = b[name].toLowerCase()
+            if (x < y) {
               return -1
             }
-
-            if (x < y) {
+            if (x > y) {
               return 1
             }
-            return 0
-          })
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        } else {
-          sortType = 'init'
-          if(tbody != null) tbody.innerHTML = tableM(sortData)
-        }
-      }*/
+            return 0 
+          } else if (sortType === 'asc') {
+            let x = a[name].toLowerCase();
+            let y = b[name].toLowerCase();
+            if (x > y) {
+              return -1;
+            }
+    
+            if (x < y) {
+              return 1;
+            }
+            return 0;
+          }
+          return 0
+        })
+      } else {
+        const name = "date"
+        result = sortData.sort((a:dataType, b:dataType) :number => {
+          if (sortType === 'init') {
+            sortFc = a[name] - b[name]
+          } else if (sortType === 'asc') {
+            sortFc = b[name] - a[name]
+          } 
+          return sortFc
+        })
+      }
+      return result
 
+    }
+
+    sortTable(clickId).then(function(sortResult) {
+      if(tbody != null && sortType === 'init') { 
+        sortType = 'asc'
+        tbody.innerHTML = tableInsert(sortResult)
+      } else if (tbody != null && sortType === 'asc') {
+        sortType = 'desc'
+        tbody.innerHTML = tableInsert(sortResult)
+      } else if (tbody != null && sortType === 'desc') {
+        sortType = 'init'
+        tbody.innerHTML = tableOrig
+      }
       existItemId = clickId
-    }
+    })
   }
 </script>
